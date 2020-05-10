35c5dc7b0b6844f28ce3ef5478725f87aeef1169
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-10 21:40:46 +0000

    test passed 'fizzbuzz.rb' refactor

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return "fizzbuzz"
-  elsif number % 3 == 0
-    return "fizz"
-  elsif number % 5 == 0
-    return "buzz"
+def fizzbuzz(n)
+  if n % 15 == 0
+    "fizzbuzz"
+  elsif n % 3 == 0
+    "fizz"
+  elsif n % 5 == 0
+    "buzz"
   else
-    return number
+    n
   end
 end
