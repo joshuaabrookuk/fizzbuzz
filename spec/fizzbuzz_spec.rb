95534d907f6377f70f5a2c3d514583f6ca974072
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-10 20:55:32 +0000

    rspec fails 'expected buzz when passes 5, returns 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,16 +4,20 @@ describe 'fizzbuzz' do
     it 'returns 1 when passed 1' do 
         expect(fizzbuzz(1)).to eq 1
     end 
-end 
-
-describe 'fizzbuzz' do 
+    
     it 'returns 2 when passed 2' do 
         expect(fizzbuzz(2)).to eq 2
     end 
-end
-
-describe 'fizzbuzz' do 
+    
     it 'returns "fizz" when passed 3' do 
         expect(fizzbuzz(3)).to eq "fizz"
+    end 
+    
+    it 'returns 4 when passed 4' do 
+        expect(fizzbuzz(4)).to eq 4
+    end
+    
+    it 'returns "Buzz" when passed 5' do 
+        expect(fizzbuzz(5)).to eq "buzz"
     end 
 end 
