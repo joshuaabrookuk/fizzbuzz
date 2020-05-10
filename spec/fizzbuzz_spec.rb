9446d17b3076e546a2885313671c2e8305d613d7
Tim <timcpb@hotmail.com>
2020-05-10 20:35:13 +0000

    Wrote failing test 'when given 2, returns 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-    it 'returns 1 when passed 1' do 
-        expect(fizzbuzz(1)).to eq 1
+    it 'returns 2 when passed 2' do 
+        expect(fizzbuzz(2)).to eq 2
     end 
 end 
