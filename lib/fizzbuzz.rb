9b0f575f44dbdfd3bda61714157fd406d4c9ef48
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-10 20:42:51 +0000

    rspec fails 'returns 3 when given 'fizz''

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,3 @@
 def fizzbuzz(number)
-    return 1
+    return number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-    it 'returns 2 when passed 2' do 
-        expect(fizzbuzz(2)).to eq 2
+    it 'returns "fizz" when passed 3' do 
+        expect(fizzbuzz(3)).to eq "fizz"
     end 
 end 
